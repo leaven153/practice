7e81aad2f23982d1a31d9d7b433d1a00e2a51a38
leaven153 <busmoja@naver.com>
2023-03-30 01:22:16 +0900

    ing

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+import FetchWrapper from "./fetch-wrapper.js";
 
 const form = document.querySelector("#todo-form");
+const btn = document.querySelector("#button-add");
+const tit = document.querySelector("#todo-title");
+const cat = document.querySelector("#todo-category");
+const list = document.querySelector("#todos-list");
+
+const API = new FetchWrapper("");
